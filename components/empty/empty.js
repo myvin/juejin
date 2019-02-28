dfae230c073735ccfb07e2e6585bf2bd47a7de12
myvin <851399101@qq.com>
2019-02-28 14:10:19 +0800

    update empty component and sharePost page

--- components/empty/empty.js
+++ components/empty/empty.js
@@ -5,6 +5,14 @@ Component({
       type: String,
       value: '/img/logo_gray.png',
     },
+    imgWidth: {
+      type: Number,
+      value: 150
+    },
+    imgHeight: {
+      type: Number,
+      value: 120
+    },
     tip: {
       type: String,
       value: '这里空空的什么都没有呢...',
